d8bc6cb6cffe7528686f1150aa2728b5df7dc3fd
Lucas Santos <lucas.faria@viannasempre.com.br>
2022-02-04 20:15:36 -0300

    Projeto Rotativa.Net6

---
+++ Rotativa.AspNetCore/Rotativa.Net6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Rotativa.Net6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
