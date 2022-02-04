869b188df9e24cb9c39cd27fca3bf94886b9343b
Lucas Santos <lucas.faria@viannasempre.com.br>
2022-02-04 19:15:34 -0300

    Projeto Rotativa.NetCore3.1

---
+++ Rotativa.AspNetCore/Rotativa.NetCore3.1/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace Rotativa.NetCore3_1.Models
+{
+    public class Client
+    {
+        public string Cpf { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Telephone { get; set; }
+    }
+}
---
+++ Rotativa.AspNetCore/Rotativa.NetCore3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Rotativa.NetCore3_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
